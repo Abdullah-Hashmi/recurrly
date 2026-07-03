3d0e4681c73b3fe22f3859a200c2f854affca893
Syed Abdullah <syedabdullah.developer@gmail.com>
2026-07-03 19:21:18 +0500

    fixes

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,8 @@ export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-background">
 
-            <Link href="/onboarding" className="mt-4 bg-primary text-white p-4">Go to onboarding</Link>;
-            <Link href="/sign-in" className="mt-4 bg-primary text-white p-4">Sign in</Link>;
+            <Link href="/onboarding" className="mt-4 bg-primary text-white p-4">Go to onboarding</Link>
+            <Link href="/sign-in" className="mt-4 bg-primary text-white p-4">Sign in</Link>
             <Link href="/sign-up" className="mt-4 bg-primary text-white p-4">Create account</Link>
         </View>
     );
